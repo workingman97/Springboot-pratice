f41cbc1849a6ef796c4c8c2cd8832834564522d3
workingman97 <1417000588@qq.com>
2023-04-17 19:07:06 -0700

    first commit

---
+++ src/main/java/net/codejava/AppController.java
@@ -0,0 +1,84 @@
+package net.codejava;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AppController {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@GetMapping("")
+	public String viewHomePage() {
+		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String showSignUpForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup_form";
+	}
+	
+	@PostMapping("/process_register")
+	public String processRegister(User user) {
+	    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	    String encodedPassword = passwordEncoder.encode(user.getPassword());
+	    user.setPassword(encodedPassword);
+	     
+	    repo.save(user);
+	     
+	    return "register_success";
+	}
+	
+	@GetMapping("/list_users")
+	public String listUsers(Model model) {
+	    List<User> listUsers = repo.findAll();
+	    model.addAttribute("listUsers", listUsers);
+	     
+	    return "users";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteUser(@PathVariable("id") long id, Model model) {
+	    User user = repo.findById(id)
+	      .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+	    repo.delete(user);
+	    List<User> listUsers = repo.findAll();
+	    model.addAttribute("listUsers", listUsers);
+	     
+	    return "users";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String showUpdateForm(@PathVariable("id") long id, Model model) {
+	    User user = repo.findById(id)
+	      .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+	    
+	    model.addAttribute("user", user);
+	    return "update-user";
+	}
+	
+	@PostMapping("/update/{id}")
+	public String updateUser(@PathVariable("id") long id, User user,  Model model) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	    String encodedPassword = passwordEncoder.encode(user.getPassword());
+	    user.setPassword(encodedPassword);
+	        
+	    repo.save(user);
+	    List<User> listUsers = repo.findAll();
+	    model.addAttribute("listUsers", listUsers);
+	     
+	    return "users";
+	}
+
+}
